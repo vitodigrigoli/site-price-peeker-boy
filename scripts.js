d7dea322b85cfadec6ff0afad2429b1332b021c9
Vito Di Grigoli <73437231+vitodigrigoli@users.noreply.github.com>
2024-01-24 00:57:58 +0100

    animation and fix spacing/font size

--- scripts.js
+++ scripts.js
@@ -2,3 +2,19 @@ const faq = document.querySelectorAll('.faq__list__item')
 console.log(faq)
 
 faq.forEach( item => item.addEventListener('click', () => item.classList.toggle('faq__list__item--open')))
+
+
+const watch = document.querySelectorAll('.watch')
+const intersection_observer = new IntersectionObserver( (items) =>{
+	items.forEach( item => {
+		
+		if( item.isIntersecting){
+			item.target.classList.add('in-view')
+		}
+		else{
+			item.target.classList.remove('in-view')
+		}
+	})
+}, { threshold: 0.1} )
+
+watch.forEach( el => intersection_observer.observe(el))
