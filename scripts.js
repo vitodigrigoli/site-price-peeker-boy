35092cd2a8a70880ba1b92b7107df965fc3f30f0
Vito Di Grigoli <73437231+vitodigrigoli@users.noreply.github.com>
2024-01-24 01:25:05 +0100

    compression video

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
 const faq = document.querySelectorAll('.faq__list__item')
-console.log(faq)
-
 faq.forEach( item => item.addEventListener('click', () => item.classList.toggle('faq__list__item--open')))
 
 
